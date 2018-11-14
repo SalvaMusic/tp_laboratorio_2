d1dc03a4027f836c5398ae30e12ba8513a4bcbd9
Salva <salva.7693@gmail.com>
2018-11-14 13:48:47 -0300

    TP4

--- Pedrozo.Salvador.2C.TP4/Pedrozo.Salvador.2C.TP4/Entidades/Correo.cs
+++ Pedrozo.Salvador.2C.TP4/Pedrozo.Salvador.2C.TP4/Entidades/Correo.cs
@@ -3,11 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Entidades
 {
-    public class Correo 
+    public class Correo : IMostrar<List<Paquete>>
     {
+        private List<Paquete> paquetes;
 
+        public Correo()
+        {
+            this.Paquetes = new List<Paquete>();
+        }
+
+        public void FinEntregas()
+        {
+
+        }
+
+        public string MostrarDatos(IMostrar<List<Paquete>> elementos)
+        {
+            List<Paquete> l = (List<Paquete>)((Correo)elementos).paquetes;
+
+            // Su código            
+            return "";
+        }
+
+        public static Correo operator +(Correo c, Paquete p)
+        {
+            foreach(Paquete paquete in c.Paquetes)
+            {
+                if (paquete == p)
+                {
+                    //TrackingIdRepetidoException.
+                    return c;
+                }
+            }
+            c.Paquetes.Add(p);
+
+            Thread hilo = new Thread(p.MockCicloDeVida);
+            //c.Paquetes. += hilo.;
+            
+            return c;
+        }
+
+        public List<Paquete> Paquetes
+        {
+            get
+            {
+                return this.paquetes;
+            }
+            set
+            {
+                this.paquetes = value;
+            }
+        }
     }
 }
--- Pedrozo.Salvador.2C.TP4/Pedrozo.Salvador.2C.TP4/Entidades/Paquete.cs
+++ Pedrozo.Salvador.2C.TP4/Pedrozo.Salvador.2C.TP4/Entidades/Paquete.cs
@@ -3,23 +3,96 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Entidades
 {
     public delegate void DelegadoEstado(object sender, EventArgs e);
     public class Paquete : IMostrar<Paquete>
     {
+        private string direccionEntrega;
+        private string trackingID;
+        private EEstado estado;
         public event DelegadoEstado InformaEstado;
 
         public Paquete(string direccionEntrega, string trackingId)
         {
-            
+            this.DireccionEntrega = direccionEntrega;
+            this.TrackingID = trackingID;
+            this.Estado = EEstado.Ingresado;
         }
-            
+
         public string MostrarDatos(IMostrar<Paquete> elemento)
         {
-            throw new NotImplementedException();
+            return String.Format(""/*{0} para {1}", elemento.trackingID, elemento.direccionEntrega*/);
         }
+
+        public override string ToString()
+        {
+            return MostrarDatos(this);
+        }
+
+        public void MockCicloDeVida()
+        {
+            do
+            {
+                Thread.Sleep(10000);
+                this.Estado++;
+                InformaEstado(this,EventArgs.Empty);
+            } while (this.Estado == EEstado.Entregado);
+            
+        }
+
+        public static bool operator ==(Paquete p1, Paquete p2)
+        {
+            if(p1.trackingID == p2.trackingID)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Paquete p1, Paquete p2)
+        {
+            return !(p1 == p2);
+        }
+
+        public string DireccionEntrega
+        {
+            get
+            {
+                return this.direccionEntrega;
+            }
+            set
+            {
+                this.direccionEntrega = value;
+            }
+        }
+
+        public string TrackingID
+        {
+            get
+            {
+                return this.trackingID;
+            }
+            set
+            {
+                this.trackingID = value;
+            }
+        }
+
+        public EEstado Estado
+        {
+            get
+            {
+                return this.estado;
+            }
+            set
+            {
+                this.estado= value;
+            }
+        }
+
 
         public enum EEstado
         {
